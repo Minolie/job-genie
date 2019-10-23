62751aaf0edb3bc240b274323e651609a77a5b5d
Minolie Fernando <minolifdo@hotmail.com>
2019-10-23 21:24:48 +0530

    Add files via upload

---
+++ src/test/java/com/job/genie/jobgenie/JobGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.genie.jobgenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
